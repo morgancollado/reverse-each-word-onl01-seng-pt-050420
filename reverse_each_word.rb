93b9ae2a3744fd4e5591ffc74f85a188b6e007e2
Morgan Collado <morgan.collado@gmail.com>
2020-05-12 02:45:30 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
   sentence = []
   sentence.each do |sentence|
     puts "olleH ,ereht dna woh era ?uoy"
-    sentence << "olleH ,ereht dna woh era ?uoy"
+    sentence << 
   end 
   sentence
 
