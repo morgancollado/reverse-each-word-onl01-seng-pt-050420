ff1460f1793a33e97561af699c8ad58cb0826313
Morgan Collado <morgan.collado@gmail.com>
2020-05-12 03:11:02 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,9 @@
 def reverse_each_word(sentence)
   new_sentence = []
   array = sentence.split
-  sentence.each do |sentence|
-  puts sentence.reverse.
-    sentence << sentence.reverse
+  array.each do |array|
+  puts array.reverse.
+    new_sentence << sentence.reverse
   end 
   sentence
 end 
